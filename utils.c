32444d643dfcc6875bc840913ae7e0fec38c6dc5
Alaa Abdelmoneim <alaa.saedy.eg@gmail.com>
2023-07-22 10:28:55 -0700

    C - creating printf

--- utils.c
+++ utils.c
@@ -1,15 +1,16 @@
 #include "main.h"
+
 /**
  * is_printable - Evaluates if a char is printable
  * @c: Char to be evaluated.
  *
  * Return: 1 if c is printable, 0 otherwise
  */
-
 int is_printable(char c)
 {
 	if (c >= 32 && c < 127)
 		return (1);
+
 	return (0);
 }
 
@@ -20,18 +21,19 @@ int is_printable(char c)
  * @ascii_code: ASSCI CODE.
  * Return: Always 3
  */
-
 int append_hexa_code(char ascii_code, char buffer[], int i)
 {
 	char map_to[] = "0123456789ABCDEF";
-
 	/* The hexa format code is always 2 digits long */
 	if (ascii_code < 0)
 		ascii_code *= -1;
+
 	buffer[i++] = '\\';
 	buffer[i++] = 'x';
+
 	buffer[i++] = map_to[ascii_code / 16];
 	buffer[i] = map_to[ascii_code % 16];
+
 	return (3);
 }
 
@@ -41,11 +43,11 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
  *
  * Return: 1 if c is a digit, 0 otherwise
  */
-
 int is_digit(char c)
 {
 	if (c >= '0' && c <= '9')
 		return (1);
+
 	return (0);
 }
 
@@ -56,13 +58,13 @@ int is_digit(char c)
  *
  * Return: Casted value of num
  */
-
 long int convert_size_number(long int num, int size)
 {
 	if (size == S_LONG)
 		return (num);
 	else if (size == S_SHORT)
 		return ((short)num);
+
 	return ((int)num);
 }
 
@@ -73,12 +75,12 @@ long int convert_size_number(long int num, int size)
  *
  * Return: Casted value of num
  */
-
 long int convert_size_unsgnd(unsigned long int num, int size)
 {
 	if (size == S_LONG)
 		return (num);
 	else if (size == S_SHORT)
 		return ((unsigned short)num);
+
 	return ((unsigned int)num);
 }
